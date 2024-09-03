150db338eb527cd115ef6102494f023d90650ed1
xrcuo <836447856@qq.com>
2024-09-03 11:59:22 +0000

    (更新启动脚本，优化配置文件备份，调整日志级别设置，修改网站主机和端口配置，优化Kestrel端点设置，调整主题颜色，优化数据库备份，优化日志记录，优化网站布局和内容宽度，优化导航栏和菜单显示)

--- start.sh
+++ start.sh
@@ -2,59 +2,91 @@
 
 # 定义EasyBot配置文件路径
 EASYBOT_PATH=/data/appsettings.json
-# 判断配置文件是否存在
-if [ ! -f "$EASYBOT_PATH" ]; then
-  # 如果不存在，则设置默认的WEB_HOST
-  : ${WEB_HOST:='http://0.0.0.0:5000'}
-  # 创建配置文件
+# 初始化EasyBot配置文件
+INIT() {
   cat <<EOF > $EASYBOT_PATH
 {
   "Logging": {
     "LogLevel": {
       "Default": "Information",
       "Microsoft": "Warning",
-      "Microsoft.Hosting.Lifetime": "Information",
-      "Grpc": "Warning"
+      "Microsoft.Hosting.Lifetime": "Information"
     }
   },
   "AllowedHosts": "*",
-  "ProSettings": {
-    "NavTheme": "realDark",
-    "HeaderHeight": 48,
-    "Layout": "side",
-    "ContentWidth": "Fixed",
-    "FixedHeader": true,
-    "FixSiderbar": true,
-    "Title": "EasyBot",
-    "IconfontUrl": null,
-    "PrimaryColor": "purple",
-    "ColorWeak": false,
-    "SplitMenus": false,
-    "HeaderRender": true,
-    "FooterRender": true,
-    "MenuRender": true,
-    "MenuHeaderRender": true
+  "ServerOptions": {
+    "Host": "$SERVER_HOST",
+    "Port": $SERVER_PORT,
+    "HeartbeatInterval": "$HEARTBEAT_INTERVAL"
   },
   "Kestrel": {
     "Endpoints": {
       "web_app": {
         "Url": "$WEB_HOST",
         "Protocols": "Http1"
-      },
-      "grpc": {
-        "Url": "http://0.0.0.0:5001",
-        "Protocols": "Http2"
       }
     }
+  },
+  "ProSettings": {
+    "NavTheme": "light",
+    "HeaderHeight": 48,
+    "Layout": "side",
+    "ContentWidth": "Fluid",
+    "FixedHeader": true,
+    "FixSiderbar": true,
+    "Title": "EasyBot",
+    "PrimaryColor": "daybreak",
+    "ColorWeak": false,
+    "SplitMenus": false,
+    "HeaderRender": true,
+    "FooterRender": true,
+    "MenuRender": true,
+    "MenuHeaderRender": true
   }
 }
 EOF
-fi
+}
+
+appsettings() {
+  if [ ! -f "$EASYBOT_PATH" ]; then
+    : ${WEB_HOST:='http://0.0.0.0:5000'}
+
+    : ${HEARTBEAT_INTERVAL:='0.00:02:00'}
+
+    : ${SERVER_HOST:='0.0.0.0'}
+
+    : ${SERVER_PORT:='26990'}
+    INIT
 
 
-rm -rf /data/wwwroot  # 删除/data/wwwroot目录
-# 将/app/EasyBot/wwwroot*目录复制到/data/wwwroot目录
-cp -a /app/EasyBot/wwwroot* /data/wwwroot
+  fi
+}
+ 
+BACKUP_PATH=/data/backup
+backup_source(){
+  if [ ! -f "$BACKUP_PATH/appsettings.json.bak" ]; then
+     mkdir -p $BACKUP_PATH
+     cp -rp $EASYBOT_PATH $BACKUP_PATH/appsettings.json.bak
+     cp -rp /data/EasyBot.db $BACKUP_PATH/EasyBot.db.bak
+     cp -a /data/options/* $BACKUP_PATH/options
+     cp -a /app/EasyBot/dp/* $BACKUP_PATH/dp
+     rm  /data/appsettings.json
+     echo -e "(配置文件已备份)"
+  else   
+     echo -e "(配置文件已备份，使用默认配置文件)"
+  fi
 
-# 运行EasyBot
-/app/EasyBot/EasyBot
+}
+
+main(){
+  backup_source
+  appsettings
+  rm -rf /data/wwwroot  # 删除/data/wwwroot目录
+  # 将/app/EasyBot/wwwroot*目录复制到/data/wwwroot目录
+  cp -a /app/EasyBot/wwwroot* /data/wwwroot
+
+  # 运行EasyBot
+  /app/EasyBot/EasyBot
+}
+
+main
